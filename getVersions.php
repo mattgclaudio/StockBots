07e566766498835260153e378caf05b5a9f95bc4
mattgclaudio <mattgclaudio@gmail.com>
2020-11-17 08:13:28 +0000

    client code to get package versions

---
+++ getVersions.php
@@ -0,0 +1,20 @@
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function getUpdate() {
+
+	$client = new rabbitMQClient("Deployment.ini","deploymentServer");
+
+	$request = array();
+	$request['type'] = "versions";
+
+	$retObj = $client->send_request($request);
+	
+	print_r($retObj['versions']);
+}
+
+getUpdate();
+
+
