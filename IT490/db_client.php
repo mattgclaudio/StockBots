533d206e416fe89049bc6f5c4409ede76bbb68c1
nartin9 <31773564+nartin9@users.noreply.github.com>
2020-12-10 12:39:16 -0500

    Update db_client.php

--- IT490/db_client.php
+++ IT490/db_client.php
@@ -4,6 +4,13 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
+function updateLog($errmsg) {
+	# with this a+ opening mode we APPEND to this existing logbook
+	$newentry = fopen("/home/matt/logbook.txt", "a+");
+	fwrite($newentry, $errmsg ."/n");
+	fclose($newentry);
+}
+
 function doItAll($a, $b) {
 $client = new rabbitMQClient("db_server.ini","dbServer");
 if (isset($argv[1]))
@@ -26,6 +33,7 @@ $response = $client->send_request($request);
 #echo "client received response: ".PHP_EOL;
 #print_r($response);
 #echo "\n\n";
+updateLog($response['message'])
 return $response['message'];
 #echo $argv[0]." END".PHP_EOL;
 
