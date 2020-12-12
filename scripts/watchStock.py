68f59fc7a0b2c6a45fcdde76a1df6aa636ea442d
Matt Claudio <mattgclaudio@gmail.com>
2020-12-12 13:15:43 -0500

    last thing that was deleted when i want HAM with rm

--- scripts/watchStock.py
+++ scripts/watchStock.py
@@ -2,7 +2,25 @@
 
 from yahoo_fin.stock_info import *
 import configparser
-import sys
+import sys, os
+from twilio.rest import Client
+
+# send a text message from our Twilio number to the user about the stock
+# striking their set price, currently only
+# texts me, would have to collect phone numbers,
+# store them in keyring.ini and then pull based on uid
+def send_alert(alert):
+    account_sid = AC4774928d3febf2ab1f54f8df5b028115
+    auth_token = 6950d7afe113961ccef0420fdda26ef3
+
+    client = Client(account_sid, auth_token)
+    
+    message = client.messages \
+                .create(
+                     body=alert,
+                     from_='+18048054083',
+                     to='+19739081950'
+                 )
 
 
 def check_price(ticker, entered_strike_price):
@@ -27,19 +45,22 @@ def watch_stock(uid):
             strike_price = config[header]['price']
             flag = check_price(ticker_name, int(strike_price))
             if flag:
-                return_message += ticker_name + \
+                new_alert = return_message += ticker_name + \
                         " has struck below your set price of $" + \
                         strike_price + " at $" + \
-                        str(get_live_price(ticker_name)) + "\n"
+                        str(get_live_price(ticker_name))
+                send_alert(new_alert)
+                
+                return new_alert
 
             else:
-                return_message += " Still no strike for stock " + \
+                def_message += " Still no strike for stock " + \
                         ticker_name + " at price " + \
                         strike_price + ", current price is $" + \
                         str(get_live_price(ticker_name)) + "\n\n"
-
-
-    return return_message
+                
+                return def_message
+        
 
 
 print(watch_stock(sys.argv[1]))
