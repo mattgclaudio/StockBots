35db452a547e2624438140bc99e4eef29835d681
nartin9 <31773564+nartin9@users.noreply.github.com>
2020-12-16 19:27:48 -0500

    Update config.php

--- IT490/config.php
+++ IT490/config.php
@@ -13,6 +13,5 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
 if($link === false){
 	die("ERROR: Could not connect, " . mysqli_connect_error());
-
 }
 ?>
