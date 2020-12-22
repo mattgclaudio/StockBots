382e94f0b8f3de7fbcb954004c2be4f93fc6cc45
mattgclaudio <mc824@njit.edu>
2020-12-22 14:28:41 -0500

    Update and rename rabbitMq setup script to setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+sudo apt-get install -y git php rabbitmq-server php-amqp php-amqplib 
+sudo rabbitmq-plugins enable rabbitmq_management
+
+echo "please restart"
+
+# run ssh-keygen 
+
+# for dmz, `python3 -m venv /path/to/new/virtual/environment` create a new venv, php install 
+# tensorflow, yahoo-fin, alapaca, ....
