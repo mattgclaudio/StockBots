2bff0cd53d506028ada0fb8ff401756872fffcc1
mattgclaudio <mc824@njit.edu>
2020-12-28 20:34:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-sudo apt-get install -y git php rabbitmq-server php-amqp php-amqplib apache2 vim 
+sudo apt-get install -y git php rabbitmq-server php-amqp php-amqplib apache2 vim certbot
 sudo rabbitmq-plugins enable rabbitmq_management
 
 echo "please restart"
